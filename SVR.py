641293c4931d850faf836047b5fc48c514303b2a
Diana Gao <47991207+DianaGao@users.noreply.github.com>
2019-05-03 17:06:32 -0400

    Update SVR.py
    added the visualization

--- SVR.py
+++ SVR.py
@@ -20,9 +20,34 @@ sc = StandardScaler()
 X = sc.fit_transform(x)
 Y = sc.fit_transform(y)
 
+# visualize the scaling 
+plt.scatter(x, y , c = "orange")
+plt.scatter(X, Y, c  = "red")
+plt.title("Scatter of the Level VS salary")
+plt.xlabel("Salary")
+plt.ylabel("Level")
+plt.show()
+
 # train the SVR model
 from sklearn.svm import SVR
 model = SVR(kernel = "rbf")
-model,fit(X, Y)
+model.fit(X, Y)
 
-# predict the 
+# predict the salary
+y_pred = model.predict(sc_x.fit_transform(6.5))
+y_pred2 = sc_y.inverse_transform(y_pred)
+
+# Create the data visualization of the scaled dataset
+plt.scatter(X, Y , c = "red")
+plt.plot(X, model.predict(X) , c = "blue")
+plt.title("SVR model")
+plt.xlabel("Salary")
+plt.ylabel("Level")
+plt.show()
+
+# Create the visualization of the orifinal dataset
+plt.scatter(x, y , c = "orange")
+plt.title("Scatter of the Level VS salary")
+plt.xlabel("Salary")
+plt.ylabel("Level")
+plt.show()
